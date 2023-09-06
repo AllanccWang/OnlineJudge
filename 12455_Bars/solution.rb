76b15bbf9b33b20741696a2b1c71e641a25596c7
Allan-D <77521070+AllanccWang@users.noreply.github.com>
2023-09-06 15:32:41 +0800

    Create solution.rb

---
+++ 12455_Bars/solution.rb
@@ -0,0 +1,14 @@
+#UVa 12455 Bars
+#s : length of the bar we want to obtain
+#n : number of bars we have
+#a : each test case representing the length of the bars
+def Bars(s,n,a)
+  dp=[-1]*(s+1)
+  dp[0]=1
+  for i in (0...n)
+    for j in (s..a[i]).step(-1)
+      dp[j]=1 if dp[j-a[i]]==1
+		end
+	end
+  dp[s]==1?"YES":"NO"
+end
