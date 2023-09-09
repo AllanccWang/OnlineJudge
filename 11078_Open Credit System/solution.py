35e5fa53b9b4b465aa9c8026a1120d1022c6622f
Allan-D <77521070+AllanccWang@users.noreply.github.com>
2023-09-09 13:09:21 +0800

    Create solution.py

---
+++ 11078_Open Credit System/solution.py
@@ -0,0 +1,12 @@
+#UVa 11078 Open Credit System
+n=int(fin.readline())
+for cases in range(0,n):
+    p=int(fin.readline()) #number of students
+    a=[int(x) for x in fin.readline().split()]
+    diff=-1000000
+    max_grade=a[0]
+    for i in range(1,len(a)):
+        if max_grade-a[i]>diff:
+            diff=max_grade-a[i]
+        max_grade=max(max_grade,a[i])
+    print(diff,file=fout)
