9da10296fe046cf3af6150f26abdda5e6c4440d8
Allan-D <77521070+AllanccWang@users.noreply.github.com>
2023-09-09 22:38:11 +0800

    Create solution.py

---
+++ 389_Basically Speaking/solution.py
@@ -0,0 +1,16 @@
+#UVa 389 Basically Speaking
+#array for storing radix
+m=[str(i) for i in range(0,10)]+[chr(x+ord("A")) for x in range(0,6)]
+#s: the number in the base you are converting from
+#a: the base you are converting from
+#b: the base you are converting to
+def Basically_Speaking(s,b,c):
+    b,c=map(int,[b,c])
+    x=int(s,b)
+    d=[]
+    while x:
+        d+=[m[int(x)%int(c)]]
+        x//=int(c)
+    s=''.join(d[::-1])
+    if len(s)>7: s=s[-7:]
+    return s.zfill(7)
