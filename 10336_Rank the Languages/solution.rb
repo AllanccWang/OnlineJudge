a78e3917d1d1f4c583f0145c6fdaac3c49b7a14b
Allan-D <77521070+AllanccWang@users.noreply.github.com>
2023-09-14 22:11:09 +0800

    Create solution.rb

---
+++ 10336_Rank the Languages/solution.rb
@@ -0,0 +1,44 @@
+#UVa 10336 Rank the Languages
+n=fin.readline.to_i
+n.times{|cases|
+	m,n=fin.readline.split.map &:to_i
+	vis=m.times.collect{[false]*n}
+	w=[]
+	l=[]
+	m.times{
+		s=fin.readline.chomp.chars
+		w+=[s]
+		s.map{|c| l+=[c]if !l.include?(c)}
+	}
+
+	d=[[1,0],[0,1],[0,-1],[-1,0]]
+	ans=[]
+	l.each{|c|
+		cnt=0
+		(0...m).map{|i|
+		for j in (0...n)
+			if w[i][j]==c && !vis[i][j]
+				stk=[[i,j]]
+				vis[i][j]=true
+				cnt+=1
+				while stk.size!=0
+					x,y=stk[0][0],stk[0][1]
+					stk=stk[1..]
+					for dir in d
+						a,b=x+dir[0],y+dir[1]
+						if a>=0 && a<m && b>=0 && b<n && w[a][b]==c && !vis[a][b]
+							stk+=[[a,b]]
+							vis[a][b]=true
+						end
+					end
+				end
+			end
+		end
+		}
+		ans+=[[c,cnt]]
+	}
+	fout.write "World ##{cases+1}\n"
+	ans.sort_by{[-_1[1],_1[0]]}.each{
+		fout.write "#{_1[0]}: #{_1[1]}\n"
+	}
+}
